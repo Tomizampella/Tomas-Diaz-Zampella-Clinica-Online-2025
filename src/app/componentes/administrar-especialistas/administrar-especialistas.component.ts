43558c2f2f1c4953a776cf849aed7b06df4522e0
Tomas Lautaro Diaz Zampella <tomizampellarp@gmail.com>
2025-06-26 00:02:43 -0300

    cambio de estado especialista corregido

--- src/app/componentes/administrar-especialistas/administrar-especialistas.component.ts
+++ src/app/componentes/administrar-especialistas/administrar-especialistas.component.ts
@@ -2,57 +2,37 @@ import { Component, inject, signal } from '@angular/core';
 import { DatabaseService } from '../../services/database.service';
 import { SupabaseService } from '../../services/supabase.service';
 import { RealtimeChannel } from '@supabase/supabase-js';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-administrar-especialistas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './administrar-especialistas.component.html',
   styleUrl: './administrar-especialistas.component.css'
 })
 export default class AdministrarEspecialistasComponent {
 db = inject(DatabaseService);
 sb = inject(SupabaseService);
-especialistas = signal<any>([]);
-canalEspecialistas: RealtimeChannel | null = null;
+especialistas: any[] = [];
 
-constructor() {
-  this.db.traerTodosLosEspecialistas().then((data) => {
-    this.especialistas.set([...data]);
-  });
-}
 
 ngOnInit() {
-  // schema-db-changes -> schema public
-  // table-db-changes -> tabla mensajes
-  this.canalEspecialistas = this.sb.supabase.channel('table-db-changes');
-  this.canalEspecialistas.on(
-    'postgres_changes',
-    {
-      // event: * (todos), INSERT, UPDATE, DELETE
-      event: '*',
-      schema: 'public',
-      table: 'usuarios_clinica',
-      filter:'rol=eq.especialista'
-    },
-    async (cambios: any) => {
-      console.log(cambios);
-      
-      this.especialistas.update((valor_anterior) => {
-        valor_anterior.push(cambios.new);
-        return valor_anterior;
-      });
-    }
-  );
-  this.canalEspecialistas.subscribe();
+  this.obtenerEspecialistas();
 }
 
-cambiarEstadoAprobacion(id_usuario:string, cambio:boolean = false){
-this.db.cambiarEstadoEspecialista(id_usuario,cambio);
+cambiarEstadoAprobacion(especialista: any) {
+  const nuevoEstado = !especialista.aprobacion_admin;
+
+  // Cambiar el estado en Supabase
+  this.db.cambiarEstadoEspecialista(especialista.id, nuevoEstado);
+
+  // Cambiar visualmente sin refrescar desde base de datos
+  especialista.aprobacion_admin = nuevoEstado;
 }
 
-ngOnDestroy() {
-    this.canalEspecialistas?.unsubscribe();
-    console.log('Canal de especialistas cerrado');
-  }
+
+async obtenerEspecialistas(){
+this.especialistas  = await this.db.traerTodosLosEspecialistas();
+}
 
 }
